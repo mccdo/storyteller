134ae84075d145c13eaf8bfe642c38df11a2a5b6
mccdo@iastate.edu <mccdo@iastate.edu@019a0280-92a8-ca63-ec0a-0019dd3501e9>
2012-08-10 01:11:12 +0000

    new build scripts for simplifing things.

--- trunk/CMakeModules/VESModuleInstall.cmake
+++ trunk/CMakeModules/VESModuleInstall.cmake
@@ -1,18 +1,19 @@
 #This script requires these variables be defined:
-# TARGET_NAME = The name of the library or application
-# TARGET_VERSION = The version of the library or application
-# TARGET_EXPORT = The export target tag
-# PUBLIC_HEADERS = Headers to be installed
+# TARGET_NAME       = The name of the library or application
+# TARGET_VERSION    = The version of the library or application
+# TARGET_EXPORT     = The export target tag
+# TARGET_CATEGORY   = This can be: Lib, App, Plugin, Test, Example
+# PUBLIC_HEADERS    = Headers to be installed
 
 set_target_properties( ${TARGET_NAME} PROPERTIES VERSION ${TARGET_VERSION} )
 set_target_properties( ${TARGET_NAME} PROPERTIES SOVERSION ${TARGET_VERSION} )
-set_target_properties( ${TARGET_NAME} PROPERTIES PROJECT_LABEL "Lib ${TARGET_NAME}" )
+set_target_properties( ${TARGET_NAME} PROPERTIES PROJECT_LABEL "${TARGET_CATEGORY} ${TARGET_NAME}" )
 
-INSTALL(TARGETS ${TARGET_NAME}
-        EXPORT storyteller-targets
+install(TARGETS ${TARGET_NAME}
+        EXPORT ${TARGET_EXPORT}
         RUNTIME DESTINATION ${VES_INSTALL_BINDIR}
         LIBRARY DESTINATION ${VES_INSTALL_LIBDIR}
         ARCHIVE DESTINATION ${VES_INSTALL_ARCHIVEDIR})
 
-INSTALL(FILES       ${PUBLIC_HEADERS}
+install(FILES       ${PUBLIC_HEADERS}
         DESTINATION ${VES_INSTALL_INCDIR}/${TARGET_NAME})
