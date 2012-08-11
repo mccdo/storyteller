775fd16ebded9c0d72897fd3235b293473ac266a
mccdo@iastate.edu <mccdo@iastate.edu@019a0280-92a8-ca63-ec0a-0019dd3501e9>
2012-08-11 04:03:25 +0000

    more cmake simplification

--- CMakeModules/VESInstallAutoDiscoveryTools.cmake
+++ CMakeModules/VESInstallAutoDiscoveryTools.cmake
@@ -12,11 +12,11 @@ export(PACKAGE ${CMAKE_PROJECT_NAME})
 
 # Create a storytellerBuildTreeSettings.cmake file for the use from the build tree
 configure_file(${PROJECT_SOURCE_DIR}/CMakeModules/packageBuildTreeSettings.cmake.in
-  "${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/${CMAKE_PROJECT_NAME}BuildTreeSettings.cmake" @ONLY)
+    "${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/${CMAKE_PROJECT_NAME}BuildTreeSettings.cmake" @ONLY)
 configure_file(${PROJECT_SOURCE_DIR}/CMakeModules/packageConfigVersion.cmake.in
-  "${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/${CMAKE_PROJECT_NAME}ConfigVersion.cmake" @ONLY)
+    "${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/${CMAKE_PROJECT_NAME}ConfigVersion.cmake" @ONLY)
 configure_file(${PROJECT_SOURCE_DIR}/CMakeModules/Usepackage.cmake.in
-  "${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/Use${CMAKE_PROJECT_NAME}.cmake" @ONLY)
+    "${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/Use${CMAKE_PROJECT_NAME}.cmake" @ONLY)
 
 include(CMakePackageConfigHelpers)
 # Create the storytellerConfig.cmake and storytellerConfigVersion files
@@ -29,13 +29,16 @@ configure_package_config_file(
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/${CMAKE_PROJECT_NAME}Config.cmake 
               ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/${CMAKE_PROJECT_NAME}ConfigVersion.cmake
               ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/Use${CMAKE_PROJECT_NAME}.cmake
-        DESTINATION ${CMAKE_INSTALL_LIBDIR} )
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        COMPONENT development)
 
 # Install the CMake find script for storyteller
-install( FILES "extra/Find${CMAKE_PROJECT_NAME}.cmake"
+configure_file(${PROJECT_SOURCE_DIR}/CMakeModules/Findpackage.cmake.in
+    "${PROJECT_BINARY_DIR}/Find${CMAKE_PROJECT_NAME}.cmake" @ONLY)
+install(FILES "${PROJECT_BINARY_DIR}/Find${CMAKE_PROJECT_NAME}.cmake"
     DESTINATION "share/${CMAKE_PROJECT_NAME}/extra"
-    COMPONENT development
-)
+    COMPONENT development)
+    #RENAME Find${CMAKE_PROJECT_NAME}.cmake)
 
 # Install the export set for use with the install-tree
 install(EXPORT ${CMAKE_PROJECT_NAME}-targets DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT development)
