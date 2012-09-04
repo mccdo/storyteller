a44cf2eeb4c951c00bd37703c83327adaf59d2ee
mccdo@iastate.edu <mccdo@iastate.edu@019a0280-92a8-ca63-ec0a-0019dd3501e9>
2012-09-04 20:31:33 +0000

    license updates.

--- trunk/src/example/main.cxx
+++ trunk/src/example/main.cxx
@@ -1,11 +1,30 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * Copyright 2012-2012 by Ames Laboratory
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
 #include <iostream>
 
 #include <Poco/Util/Option.h>
 #include <Poco/Util/OptionSet.h>
-#include "Poco/Util/Application.h"
-#include "Poco/Util/HelpFormatter.h"
-#include "Poco/Util/AbstractConfiguration.h"
-#include "Poco/AutoPtr.h"
+#include <Poco/Util/Application.h>
+#include <Poco/Util/HelpFormatter.h>
+#include <Poco/Util/AbstractConfiguration.h>
+#include <Poco/AutoPtr.h>
 
 #include <storyteller/PresentationMaker.h>
 
